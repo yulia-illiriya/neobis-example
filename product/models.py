7200c348b8972961b2c68798687d8a412db13e57
yulia-illiriya <yulia.illiriya@gmail.com>
2023-06-29 20:57:50 +0600

    feat: add User model and Basket

--- product/models.py
+++ product/models.py
@@ -13,6 +13,11 @@ class Price(models.Model):
         
 class Photo(models.Model):
     photo_url = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
+    uploaded_at = models.DateTimeField("Фото загружено", auto_now_add=True)
+    title = models.CharField("Описание", default="Sample")
+    
+    def __str__(self):
+        return self.title
 
 class Product(models.Model):
     name_of_product = models.CharField("Product", max_length=100)
@@ -21,6 +26,7 @@ class Product(models.Model):
     created_at = models.DateTimeField("Запись создана", auto_now_add=True)
     updated_at = models.DateTimeField("Запись обновлена", auto_now_add=True)
     price = models.ForeignKey(Price, related_name="product", on_delete=models.SET_NULL, null=True)
+    amount_of_likes = models.PositiveBigIntegerField("Нравится", default=0)
     
     def __str__(self):
         return self.name_of_product
@@ -28,3 +34,9 @@ class Product(models.Model):
     class Meta:
         verbose_name = "Товар"
         verbose_name_plural = "Товары"
+
+class Basket(models.Model):
+    product = models.ForeignKey(Product, related_name="basket", on_delete=models.CASCADE)
+    created_at = models.DateTimeField("Корзина создана", auto_now_add=True)
+    is_completed = models.BooleanField("Корзина собрана?", default=False)
+    
--- profile_user/models.py
+++ profile_user/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from profile_user.managers import UserManager
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(_('username'), max_length=255, unique=True)
+    email = models.EmailField(_('email address'), null=True, blank=True)
+    phone = models.CharField(_('phone number'), max_length=30, null=True, blank=True)
+    is_active = models.BooleanField(_('active'), default=False)
+    is_staff = models.BooleanField(_('staff'), default=False)
+
+    is_verified_by_phone = models.BooleanField(_('verified'), default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+        unique_together = ('username', 'email', 'phone')
