5238c1c16ca39206df575faea178c0768ecaebc1
yulia-illiriya <yulia.illiriya@gmail.com>
2023-07-04 15:19:13 +0600

    feat: add profile view

--- profile_user/urls.py
+++ profile_user/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .services import *
+from .views import UserProfileDetailAPIView
 
 
 urlpatterns = [
     path('send-verification-code/', send_verification_code, name='send_verification_code'),
     path('verify-code/', verify_code, name="verify_code"),
     path('verify-user/', verify_user, name="verify_user"),
+    path('profile/', UserProfileDetailAPIView.as_view(), name="profile")
 ]
--- profile_user/views.py
+++ profile_user/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views import View
 from profile_user.services import send_verification_code, verify_code
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
+from .models import UserProfile
+from .serializers import UserProfileSerializer
 
 class SendVerificationCodeView(View):
     def post(self, request):
@@ -9,4 +15,14 @@ class SendVerificationCodeView(View):
 class VerifyCodeView(View):
     def post(self, request):
         return verify_code(request)
+    
+
+class UserProfileDetailAPIView(generics.RetrieveUpdateAPIView):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        user = self.request.user
+        return get_object_or_404(UserProfile, user=user)
 
